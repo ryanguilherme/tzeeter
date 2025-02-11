876d39585847977df7c1f870b05fc11cb1dd2b58
ryanguilherme <ryanguilhermetbt@gmail.com>
2025-02-11 17:55:18 -0300

    feature(main.js): auto-resize for text areas script

--- scripts/main.js
+++ scripts/main.js
@@ -67,3 +67,13 @@ document.addEventListener("DOMContentLoaded", function () {
         postDialog.close();
     });
 })();
+
+document.querySelectorAll("textarea").forEach(function(textarea) {
+    textarea.style.height = textarea.scrollHeight + "px";
+    textarea.style.overflowY = "hidden";
+
+    textarea.addEventListener("input", function() {
+        this.style.height = "auto";
+        this.style.height = this.scrollHeight + "px";
+    });
+});
